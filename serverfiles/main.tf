fd782505e51c8d29a4d4d2bbcc95e1e7de0bc496
sanumounika <115688300+sanumounika@users.noreply.github.com>
2023-04-20 20:38:54 +0530

    Update main.tf

--- serverfiles/main.tf
+++ serverfiles/main.tf
@@ -18,7 +18,22 @@ resource "aws_instance" "test-server" {
   provisioner "local-exec" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
 		}
-  provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/health-care-project/serverfiles/healthcareplaybook.yml"
+  provisioner "remote-exec" {
+	inline = [
+        "sudo apt update -y",
+              "sudo apt install docker.io -y",
+              "sudo snap install microk8s --classic",
+              "sudo sleep 30",
+              #"sudo usermod -aG microk8s $USER",
+             #"sudo chown -f -R $USER ~/.kube",
+             # "sudo microk8s status --wait-ready",
+             # "sudo microk8s enable dns ingress",
+              "sudo microk8s status",
+              "sudo microk8s kubectl create deployment health-deploy --image=mouni1arjun/healthcare:latest",
+              "sudo microk8s kubectl expose deployment health-deploy --port=8082 --type=NodePort",
+              "sudo microk8s kubectl get svc",
+              "sudo echo Public IP Address of the Instance",
+              "sudo curl http://checkip.amazonaws.com",
+  ]
   } 
 }
