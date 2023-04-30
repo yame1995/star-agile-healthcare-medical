02102974330d893cb955661e63bbafa366d564cd
fatima <110208921+Fatima-Ishrath021@users.noreply.github.com>
2023-04-30 11:25:31 +0530

    Update provider.tf

--- serverfiles/provider.tf
+++ serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6A5CSFGY5MAXZX6F"
-  secret_key = "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"
+  access_key = "KIA6A5CSFGY5MAXZX6F"
+  secret_key = "LUYjLCKTO6kEpfT8pcN5CBjbwpPUEtdlEw1UlLx"
 }
