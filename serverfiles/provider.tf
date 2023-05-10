19b1cd3f6b3cd100bf90831dad4f0c60a65c67b1
yame1995 <120810620+yame1995@users.noreply.github.com>
2023-05-10 08:14:54 -0700

    Update provider.tf

--- serverfiles/provider.tf
+++ serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "KIA6A5CSFGY5MAXZX6F"
-  secret_key = "LUYjLCKTO6kEpfT8pcN5CBjbwpPUEtdlEw1UlLx"
+  access_key = "AKIAZFNSR3ZDG75BMJND"
+  secret_key = "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"
 }
